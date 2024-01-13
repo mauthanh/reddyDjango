c5883696fc92f50d4a3bdd25e33b1f685205f0b1
mauthanh <thanh.truongmau@gmail.com>
2024-01-13 07:38:17 +0700

    create second application, change urls

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+
+def display(request):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def displaydatetime(request):
+    dt = datetime.now()
+    s = "<b>Current date time: </b>" + str(dt)
+    return HttpResponse(s)
--- reddyDjango/settings.py
+++ reddyDjango/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'firstApp'
+    'quoteApp',
+    'firstApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
